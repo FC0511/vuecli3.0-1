c112a7b9bb086d8b4142dad16efe1e910274b1b8
Fc <1032733797@qq.com>
2020-01-30 22:09:30 +0800

    路由配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/img/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/img/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App"/>
+		<button @click="handleClick">返回上一页</button>
+		<button @click="handleClick(1)">跳转</button>
+		<button @click="handleClick(2)">替换</button>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'home',
+        components: {
+            HelloWorld
+        },
+        methods: {
+            handleClick(type) {
+                if (type === 1) {
+                    this.$router.push({
+												// path: `/argu/123`, // 24行等同于25-26
+												name: 'argu',
+												params: {
+												    name: '1323'
+												}
+												/*query: {
+												    name: 'zhangSan'
+												}*/
+										});
+                } else if (type === 2) {
+                    this.$router.push('/parent');
+                } else {
+                    this.$router.go(-1);
+                    // this.$router.back();
+								}
+
+            }
+        }
+    }
 </script>
---
+++ src/views/email.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>email</div>
+</template>
+
+<script>
+    export default {
+        name: "email"
+    }
+</script>
+
+<style scoped>
+
+</style>
